eee6e44ba3673aa10ffc7dc6607ca811a257740a
KarolisRaulickis <kraulickis@yahoo.com>
2020-05-23 09:13:53 +0300

    Test vb push

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@ title = {{$title}} --}}
 @extends('layouts.app')
 
 @section('content')
-    <div>atejo<br>  {{ __('Test input') }}</div> 
+    <div>atejo cia<br>  {{ __('Test input') }}</div> 
 
     @if(session('success'))
     <div class="alert alert-success">
